bf444bfba053fd2b7391e0d0812f02c2138b99ba
Konstantin Knauf <knauf.konstantin@gmail.com>
2022-03-24 17:12:46 +0100

    add test that two rows are not the same

--- src/test/java/com/github/knaufk/flink/faker/FlinkFakerIntegrationTest.java
+++ src/test/java/com/github/knaufk/flink/faker/FlinkFakerIntegrationTest.java
@@ -23,22 +23,21 @@ public class FlinkFakerIntegrationTest {
     StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
 
     tEnv.executeSql(
-            "CREATE TABLE server_logs ( \n"
-                    + "    client_ip STRING,\n"
-                    + "    client_identity STRING, \n"
-                    + "    userid STRING, \n"
-                    + "    log_time AS PROCTIME(),\n"
-                    + "    request_line STRING, \n"
-                    + "    status_code STRING\n"
-                    + ") WITH (\n"
-                    + "  'connector' = 'faker', \n"
-                    + "  'fields.client_ip.expression' = '#{Internet.publicIpV4Address}',\n"
-                    + "  'fields.client_identity.expression' =  '-',\n"
-                    + "  'fields.userid.expression' =  '-',\n"
-                    + "  'fields.request_line.expression' = '#{regexify ''(GET|POST|PUT|PATCH){1}''} #{regexify ''(/search\\.html|/login\\.html|/prod\\.html|cart\\.html|/order\\.html){1}''} #{regexify ''(HTTP/1\\.1|HTTP/2|/HTTP/1\\.0){1}''}',\n"
-                    + "  'fields.status_code.expression' = '#{regexify ''(200|201|204|400|401|403|301){1}''}',\n"
-                    + "  'number-of-rows' = '2'\n"
-                    + ")");
+        "CREATE TABLE server_logs ( \n"
+            + "    client_ip STRING,\n"
+            + "    client_identity STRING, \n"
+            + "    userid STRING, \n"
+            + "    request_line STRING, \n"
+            + "    status_code STRING\n"
+            + ") WITH (\n"
+            + "  'connector' = 'faker', \n"
+            + "  'fields.client_ip.expression' = '#{Internet.publicIpV4Address}',\n"
+            + "  'fields.client_identity.expression' =  '-',\n"
+            + "  'fields.userid.expression' =  '-',\n"
+            + "  'fields.request_line.expression' = '#{regexify ''(GET|POST|PUT|PATCH){1}''} #{regexify ''(/search\\.html|/login\\.html|/prod\\.html|cart\\.html|/order\\.html){1}''} #{regexify ''(HTTP/1\\.1|HTTP/2|/HTTP/1\\.0){1}''}',\n"
+            + "  'fields.status_code.expression' = '#{regexify ''(200|201|204|400|401|403|301){1}''}',\n"
+            + "  'number-of-rows' = '2'\n"
+            + ")");
 
     TableResult tableResult = tEnv.executeSql("SELECT * FROM server_logs");
 
